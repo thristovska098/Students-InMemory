42c6f62dd071d01326e4278761c9f04fe09458e9
thristovska098 <thristovska098@gmail.com>
2021-01-28 14:13:44 +0100

    Second commit, some changes in the App class

--- HomeworkGitMaven/src/main/java/mk/iwec/App.java
+++ HomeworkGitMaven/src/main/java/mk/iwec/App.java
@@ -8,8 +8,8 @@ public class App {
 		mm.setMemory();
 		StudentsCollecton sc = mm.getSc();
 
-		// show all the students with firstName="Milica"
-		System.out.println(sc.selectName("Milica"));
+		// show all the students with lastName="Trajkovska"
+		System.out.println(sc.selectLastName("Trajkovska"));
 
 	}
 
